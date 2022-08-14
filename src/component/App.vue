e122b58ffa104abe11ba9f17a68e2a1cfd8a1e63
Hyunwoo Kang <40859447+kangprog@users.noreply.github.com>
2022-08-14 15:45:08 +0900

    [feat] search box 구현 (#2)
    - nav, header, main, footer 분리
    - search box 구현
    
    resolved: #1

---
+++ src/component/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <input type="text" v-model="input" placeholder="Search..."/>
+</template>
+
+<script setup>
+  import { ref } from "vue";
+  let input = ref("");
+</script>
+
+<style src="../style/custom_style.css" />
