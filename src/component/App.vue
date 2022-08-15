3fd95748a49004904f1bab151c8b41d047b262df
Hyunwoo Kang <40859447+kangprog@users.noreply.github.com>
2022-08-16 08:58:21 +0900

    [feat] nav bar 구현 (#4)
    - nav bar expand modal 형태로 구현
    - 임시 nav 목록 생성
    
    resolved: #3

--- src/component/App.vue
+++ src/component/App.vue
@@ -1,10 +1,60 @@
 <template>
-  <input type="text" v-model="input" placeholder="Search..."/>
+    <!-- The overlay -->
+    <div id="myNav" class="overlay">
+      <!-- Overlay content -->
+      <div class="overlay-modal">
+        <div class="modal-contents">
+          <label>Hadoop & Eco System</label>
+          <div class="modal-list">
+            <a href="#"><font size=4rem; color="white">· MIDAS Ambari</font></a>
+            <a href="#"><font size=4rem; color="white">· MEDUS Ambari</font></a>
+            <a href="#"><font size=4rem; color="white">· MALTHAEL Ambari</font></a>
+          </div>
+        </div>
+
+        <div class="modal-contents">
+          <label>BI</label>
+        </div>
+
+        <div class="modal-contents">
+          <label>Elastic Search</label>
+        </div>
+
+        <div class="modal-contents">
+          <label>Kibana</label>
+        </div>
+
+        <div class="modal-contents">
+          <label>Etc</label>
+        </div>
+
+        <!-- Button to close the overlay navigation -->
+        <a href="javascript:void(0)" class="closebtn" v-on:click="closeNav()" align="right">&times;</a>
+      </div>
+    </div>
+    <span v-on:click="openNav()"> #전체보기 </span>
+
+    <div id="header_custom">
+        <h1 id="header_h1"> Zumvigator </h1>
+    </div>
+    <input type="text" v-model="input" placeholder="Search..."/>
+
+    <div id="footer_custom">
+        Copyright ⓒ Kang Hyunwoo
+    </div>
 </template>
 
 <script setup>
   import { ref } from "vue";
   let input = ref("");
+
+  function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
 </script>
 
 <style src="../style/custom_style.css" />
